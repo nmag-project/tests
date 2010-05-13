a063db9c1c4e5c2264b34bf49cc1e7a48bb0d498
Matteo Franchin [beta] <franchin@soton.ac.uk>
2010-05-13 16:37:09 +0100

    Can compute averages of fields now.

--- devtests/nmag/physics/llg-basic/model.py
+++ devtests/nmag/physics/llg-basic/model.py
@@ -3,12 +3,12 @@ import nmesh
 from nmag import MagMaterial
 
 # Constants and fields
-M_sat = Constant("M_sat", def_on_material=True, value=Value(0.86e6))
-gamma_GG = Constant("gamma_GG", def_on_material=True, value=Value(2.210173e5))
-alpha = Constant("alpha", def_on_material=True, value=Value(0.2))
-m = SpaceField("m", [3], def_on_material=True, value=Value([1, 0, 0]))
+M_sat = Constant("M_sat", subfields=True, value=Value(0.86e6))
+gamma_GG = Constant("gamma_GG", subfields=True, value=Value(2.210173e5))
+alpha = Constant("alpha", subfields=True, value=Value(0.2))
+m = SpaceField("m", [3], subfields=True, value=Value([1, 0, 0]))
 H_ext = SpaceField("H_ext", [3], value=Value([0, 0, 1.0e5]))
-dmdt = SpaceField("dmdt", [3], def_on_material=True)
+dmdt = SpaceField("dmdt", [3], subfields=True)
 
 # Equation of motion
 llg = Equation("llg", """
@@ -22,11 +22,9 @@ ts = Timestepper("ts_llg", x='m', dxdt='dmdt', eq_for_jacobian=llg)
 # All materials
 mat_Py = MagMaterial('Py')
 
-# Load mesh
-mesh = nmesh.load("mesh.nmesh.h5")
-region_materials = [[], [mat_Py]]
-
 # Put everything together in a physical model
+mesh = nmesh.load("mesh.nmesh.h5")
+region_materials = [[], ["Py"]]
 p = Model("mumag", mesh, region_materials)
 p.add_quantity([alpha, gamma_GG, m, H_ext, dmdt])
 p.add_computation(llg)
@@ -35,9 +33,9 @@ p.build()
 
 # Now we can use the model
 f = open("model.dat", "w")
-f.write("%g " % 0 + "%g %g %g\n" % tuple(m.integrate()[0][1]))
+f.write("%g " % 0 + "%g %g %g\n" % tuple(m.compute_average()[0][1]))
 for i in range(1, 101):
   t = i*10e-12
   ts.advance_time(t)
-  f.write("%g " % t + "%g %g %g\n" % tuple(m.integrate()[0][1]))
+  f.write("%g " % t + "%g %g %g\n" % tuple(m.compute_average()[0][1]))
 f.close()
