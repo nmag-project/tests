6eda6099710a5e500b7dcd26a604ac02689940c0
Matteo Franchin [beta] <franchin@soton.ac.uk>
2010-06-25 16:36:26 +0100

    Adding example to better study memory allocation in Nmag.

---
+++ devtests/nmag/memory/bar.geo
@@ -0,0 +1,6 @@
+algebraic3d
+
+# cubes
+solid bar = orthobrick (0, 0, 0; 100.0, 30.0, 30.0) -maxh = 1.5;
+tlo bar;
+
---
+++ devtests/nmag/memory/run.py
@@ -0,0 +1,29 @@
+import nmag
+from nmag import SI,mesh
+
+mat_Py = nmag.MagMaterial(name="Py",
+                          Ms=SI(0.86e6, "A/m"),
+                          exchange_coupling=SI(13.0e-12, "J/m"),
+                          llg_gamma_G=SI(0.2211e6, "m/A s"))
+
+sim = nmag.Simulation()
+
+sim.load_mesh("bar.nmesh.h5", [("Py", mat_Py)], unit_length=SI(1e-9, "m"))
+
+import math
+angle_deg = 45
+angle_rad = angle_deg/360.*2*math.pi
+sim.set_m([math.cos(angle_rad), 0, math.sin(angle_rad)])
+sim.set_params(ts_rel_tol=2.7e-05 , ts_abs_tol=2.7e-05)
+
+dt = SI(5e-12, "s") 
+
+for i in range(0, 3):
+    time_ = dt*i
+    target_time = sim.advance_time(time_)
+
+    if i % 10 == 0:
+        sim.save_data(fields='all')
+    else:
+        sim.save_data()
+
