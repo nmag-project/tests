fbe4131338576fdaa8d051c546d5d42e91f81192
Hans Fangohr [VM Ubuntu 11.10] <fangohr@soton.ac.uk>
2011-10-23 20:06:08 +0100

    Adding test for python modules. Only tables and ply included so far (those were missing for me).

---
+++ regression/nmag/pymodules/test_python_modules_installed.py
@@ -0,0 +1,6 @@
+def test_pymodules_installed():
+	#is the ply module installed (Lex and Yacc for Python)?
+	import ply
+
+	#is the pytables module installed (Python interface to hdf5)?
+	import tables
