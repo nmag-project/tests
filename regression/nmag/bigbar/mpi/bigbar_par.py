4a2400487e5bf679e1738296a9e6ec99e67aad81
Matteo Franchin <none@none>
2011-12-06 13:59:13 +0000

    Fixing tests (they now use the new nsim.testtools framework).

--- regression/nmag/bigbar/mpi/bigbar_par.py
+++ regression/nmag/bigbar/mpi/bigbar_par.py
@@ -68,13 +68,14 @@ def out(line, header=False, file="bigbar_timings.log"):
 import commands
 host = commands.getoutput("uname -n")
 date = time.asctime()
-rev = "$Revision$"[11:-2] #This file's version
-import nsim
-import nsim.svnversion
-rev = nsim.svnversion.svnversion #global version -> more useful
-out("# Timings for the bigbar unit test\n" \
-    "# host\t Rev\t date sim\t init\t writing-data\t sim&writing-data\t total\tRev\n", header=True)
-out("%s \t%s \t%s \t%g \t%g \t%g \t%g \t%g\n" % (host, rev, date,time_simulating, time_initialising, time_writing, time_loop, time_total))
+from nsim.versions import get_version_string
+rev = get_version_string()
+
+out("# Timings for the bigbar unit test\n# host\t Rev\t date sim\t "
+    "init\t writing-data\t sim&writing-data\t total\tRev\n", header=True)
+out("%s \t%s \t%s \t%g \t%g \t%g \t%g \t%g\n"
+    % (host, rev, date,time_simulating, time_initialising, time_writing,
+       time_loop, time_total))
 
 print "=== LAM Timings ===\n",sim.get_timers()
 
--- regression/nmag/demag_twomat/test_slow_bug_ticket175.py
+++ regression/nmag/demag_twomat/test_slow_bug_ticket175.py
@@ -1,69 +1,8 @@
-import sys, os
-#setup_directories
-org_dir = os.getcwd()
-
-debug = False
-try:
-  this_dir = os.path.split(__file__)[0]
-except NameError: #thrown if not run through pytest_nsim (because __file__ is not defined)
-  this_dir = os.getcwd()
-  debug = True
-  
-out_file = os.path.join(this_dir,"test_demag_twomat1.dat")
-
-def out(msg):
-  f = open(out_file, "at")
-  f.write(msg + "\n")
-  f.close()
-
-def do_simulation():
-  import time
-  import nmag
-  from nmag import SI, mesh
-  import os
-
-  mat1 = nmag.MagMaterial(name="mat1",
-                          Ms=SI(0.8e6, "A/m"),
-                          exchange_coupling=SI(13.0e-12, "J/m"),
-                          llg_gamma_G=SI(0.2211e6, "m/A s"),
-                          llg_damping=0.5)
-
-  sim1 = nmag.Simulation(name="simulation1")
-  sim1.load_mesh("bar.nmesh.h5", 
-                [("one", mat1), ("two", mat1)],
-                unit_length=SI(1e-9,"m"))
-
-  sim2 = nmag.Simulation(name="simulation2")
-  sim2.load_mesh("bar_oneregion.nmesh.h5",
-                [("one", mat1)],
-                unit_length=SI(1e-9,"m"))
-
-  import math
-  angle_deg = 45
-  angle_rad = angle_deg/360.*2*math.pi
-
-
-
-  #Setting first sim2.set_m, then sim1.set_m works. The other way
-  #throws an error (ticket:175)
-
-  print "Setting m for sim2"
-  sim2.set_m([math.cos(angle_rad), 0, math.sin(angle_rad)])
-  print "Setting m for sim1"
-  sim1.set_m([math.cos(angle_rad), 0, math.sin(angle_rad)])
-  print "Done (this is where ticket:175 would never get to)"
-
-  sim1.advance_time(SI(1e-15,'s'))
-  print "Done2"
-  sim2.advance_time(SI(1e-15,'s'))
-  print "Done3"
+import nmag
+from nsim.testtools import run_make, DirectoryOf
 
 def test_demag():
-  os.chdir(this_dir)
-  out(os.getcwd())
-  os.system("make init >make.out 2>&1")
-  do_simulation()
-  os.chdir(org_dir)
+    run_make(__file__)
 
-if debug:
-  test_demag()
+if __name__ == "__main__":
+    test_demag()
